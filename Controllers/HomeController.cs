47334a592e00797cff67d097256bcc4949a888a5
Grant <32653173+desluOs@users.noreply.github.com>
2025-05-05 15:57:24 -0400

    Add files via upload
    testing

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GroupProject.Models;
+
+namespace GroupProject.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
